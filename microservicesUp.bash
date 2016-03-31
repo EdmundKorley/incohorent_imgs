b008c73f1842473d6ca9966f7a53fde9ee7ac663
Edmund Korley <korley.em@gmail.com>
2016-03-30 22:46:11 -0400

    Register workerService in bash script

--- microservicesUp.bash
+++ microservicesUp.bash
@@ -4,3 +4,4 @@ go run src/kVService.go &
 go run src/taskService.go 127.0.0.1:3001 127.0.0.1:3000 &
 go run src/storageService.go 127.0.0.1:3002 127.0.0.1:3000 &
 go run src/masterService.go 127.0.0.1:3003 127.0.0.1:3000 &
+go run src/workerService.go 127.0.0.1:3000 2 &
